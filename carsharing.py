cac022e8707700e97abae7559c0779e1a715dfdc
shivesh-anand <btech10218.21@bitmesra.ac.in>
2026-05-31 20:58:56 +0530

    Added get car by id api, path params, http exception, run uvicorn

--- carsharing.py
+++ carsharing.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -27,3 +28,15 @@ def get_cars(size:str|None = None, doors:int|None = None) -> list:
     if doors:
         result = [car for car in result if car["doors"] >= doors]
     return result
+
+@app.get("/api/cars/{id}")
+def car_by_id(id: int) -> dict:
+    result = [car for car in db if car["id"] == id]
+    if result:
+        return result[0]
+    else:
+        raise HTTPException(status_code=404, detail=f"No car with id {id}")
+
+
+if __name__ == "__main__":
+    uvicorn.run("carsharing:app", reload=True)
